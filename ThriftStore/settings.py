48cc266e3e8c67abba3e5cb9943272555af35b6e
Fathimath Nahla Salami E <nahlasalami321@gmail.com>
2026-06-17 03:02:39 +0400

    updation on allowed hosts

--- ThriftStore/settings.py
+++ ThriftStore/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-+k#t-zy=^lhjh2%u--s=x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['ThriftStoreApp.onrender.com']
+ALLOWED_HOSTS = [
+    'ThriftStoreApp.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
